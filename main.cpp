2603001542455e79f7b243e1a1cb0e749d2b29bb
xTheCoderx <justaguyoney@gmail.com>
2021-10-20 21:01:50 +1300

    Changed array size, added a newline where needed.

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ int main() {
     const std::string creator = "Bjarne Stroutstrup";
     std::cout << "C++ is a general purpose programming language created by " << creator << "\n";
 
-    std::cout << " C++ is:";
+    std::cout << " C++ is:" << "\n";
 
-    std::string is[10] = {"Low-level",
+    std::string is[3] = {"Low-level",
                           "Supports a variety of features",
                           "Supports move semantics, smart pointers, and automatic type inference, as well as threading, and lambda functions.",};
 
