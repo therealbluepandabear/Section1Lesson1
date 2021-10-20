c5d62ca4ba1e693a332431ae96df58c54b999b85
xTheCoderx <justaguyoney@gmail.com>
2021-10-20 21:00:43 +1300

    Added details on what C++ really is.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,17 @@
 // Section 1 - Lesson 1
 int main() {
     const std::string creator = "Bjarne Stroutstrup";
-    std::cout << "C++ is a general purpose programming language created by " << creator << std::endl;
+    std::cout << "C++ is a general purpose programming language created by " << creator << "\n";
+
+    std::cout << " C++ is:";
+
+    std::string is[10] = {"Low-level",
+                          "Supports a variety of features",
+                          "Supports move semantics, smart pointers, and automatic type inference, as well as threading, and lambda functions.",};
+
+    for (const auto &item : is) {
+        std::cout << "- " << item << "\n";
+    }
 
     return 0;
 }
